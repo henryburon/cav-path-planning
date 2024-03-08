f16658d74f929ffffbf96d61c8bc9c99ca425fe8
Henry Buron <henryburon34@gmail.com>
2024-03-07 19:02:14 -0600

    animated the plot

--- main/main.py
+++ main/main.py
@@ -1,16 +1,23 @@
 from lib.render_maze import *
 from lib.astar import *
+from matplotlib.animation import FuncAnimation
 
 
 def main():
+
    global map, start, goal, path
+
+   def update(i):
+    if i < len(path):
+        point = path[i]
+        plt.plot(point[1], point[0], marker='x', color='red', markersize=2)
 
    # capture_image()
    prepare_image()
 
    map = convert_to_binary()
    map = 255 - map # Invert the map so free spaces are white, obstacles are black
-   plt.imshow(map, cmap='Greys', interpolation='nearest')
+   # plt.imshow(map, cmap='Greys', interpolation='nearest')
 
    start = (190, 270)
    goal = (290, 270)
@@ -19,16 +26,13 @@ def main():
    path = astar(map, start, goal, heuristic)
 
    if path is not None:
-
-      # Plot the path
-      path_x = [point[1] for point in path]
-      path_y = [point[0] for point in path]
-      plt.plot(path_x, path_y, marker='x', color='red', markersize=2)
-
+      fig = plt.figure(figsize=(10, 10))
+      plt.imshow(map, cmap='Greys', interpolation='nearest')
       plt.plot(start[1], start[0], marker='o', color='green', markersize=5)
       plt.plot(goal[1], goal[0], marker='o', color='blue', markersize=5)
       plt.xticks([]), plt.yticks([])
-
+      # ani = FuncAnimation(fig, update, frames=len(path), repeat=False)
+      ani = FuncAnimation(fig, update, frames=len(path), repeat=False, interval=35)
       plt.show()
    else:
       print("No path found.")
